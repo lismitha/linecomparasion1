9a545a9e594b3695702d4f203fa2313236819a0a
lismitha <lismithaspatel@gmail>
2021-11-21 00:15:39 +0530

    adding equality file

--- linecomparasion1/line.cs
+++ linecomparasion1/line.cs
@@ -2,29 +2,48 @@
 
 namespace LineComprison
 {
-    class line
+    class Program
     {
         static void Main(string[] args)
-           
-            {
-                Console.Write("Enter the Co-rdinate p1: ");
-                Double p1 = Convert.ToDouble(Console.ReadLine());
+        {
+            Console.WriteLine("First Line");
+            Console.Write("Enter the Co-rdinate p1: ");
+            double p1 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Enter the Co-rdinate q1: ");
-                Double q1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate q1: ");
+            double q1 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Enter the Co-rdinate p2: ");
-                Double p2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate p2: ");
+            double p2 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Enter the Co-rdinate q2: ");
-                Double q2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate q2: ");
+            double q2 = Convert.ToDouble(Console.ReadLine());
 
-            double Powofpoints1 = Math.Pow(2, (p2 - p1));
-                double Powofpoints2 = Math.Pow(2, (q2 - q1));
-                double LengthofLine = Math.Sqrt((Powofpoints1 + Powofpoints2));
+            Console.WriteLine("Second Line");
+            Console.Write("Enter the Co-rdinate A1: ");
+            double A1 = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine("Length of the Line is: " + LengthofLine);
-                Console.ReadLine();
-            }
+            Console.Write("Enter the Co-rdinate B1: ");
+            double B1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate A2: ");
+            double A2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate B2: ");
+            double B2 = Convert.ToDouble(Console.ReadLine());
+
+            double PowofpointsX = Math.Pow(2, (p2 - q1));
+            double PowofpointsY = Math.Pow(2, (p2 - q1));
+            double LengthofLine1 = Math.Sqrt((PowofpointsX + PowofpointsY));
+            Console.WriteLine("Length of the Line is: " + LengthofLine1);
+
+            double PowofpointsA = Math.Pow(2, (A2 - A1));
+            double PowofpointsB = Math.Pow(2, (B2 - B1));
+            double LengthofLine2 = Math.Sqrt((PowofpointsA + PowofpointsB));
+            Console.WriteLine("Length of the Line is: " + LengthofLine2);
+
+            Console.WriteLine(LengthofLine1.Equals(LengthofLine2));
+            Console.ReadLine();
         }
     }
+}
