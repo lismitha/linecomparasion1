3516e6bc0e7fd3be2d4f10181fd7e091b52f3297
lismitha <lismithaspatel@gmail>
2021-11-20 23:34:43 +0530

    adding line comparation welcome

---
+++ linecomparasion1/line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComprison
+{
+    class welcome 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Program");
+            Console.ReadLine();
+        }
+    }
+}
