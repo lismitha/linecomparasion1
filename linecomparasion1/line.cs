78f70c2092bf271fbaf216e7de8f46411a8a0a3d
lismitha <lismithaspatel@gmail>
2021-11-20 23:56:18 +0530

    adding length finding code

--- linecomparasion1/line.cs
+++ linecomparasion1/line.cs
@@ -2,12 +2,29 @@
 
 namespace LineComprison
 {
-    class welcome 
+    class line
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the Line Comparison Program");
-            Console.ReadLine();
+           
+            {
+                Console.Write("Enter the Co-rdinate p1: ");
+                Double p1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the Co-rdinate q1: ");
+                Double q1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the Co-rdinate p2: ");
+                Double p2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the Co-rdinate q2: ");
+                Double q2 = Convert.ToDouble(Console.ReadLine());
+
+            double Powofpoints1 = Math.Pow(2, (p2 - p1));
+                double Powofpoints2 = Math.Pow(2, (q2 - q1));
+                double LengthofLine = Math.Sqrt((Powofpoints1 + Powofpoints2));
+
+                Console.WriteLine("Length of the Line is: " + LengthofLine);
+                Console.ReadLine();
+            }
         }
     }
-}
